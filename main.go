29f96abd0907ba5b3b7a462457c8c73c6ab10d03
Thiago Kufa <tkufa.code@gmail.com>
2024-08-18 00:00:37 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/ThiagoKufa/estudo_go/router"
+import (
+	"github.com/ThiagoKufa/estudo_go/config"
+	"github.com/ThiagoKufa/estudo_go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("Config initialization erro : %v", err)
+		return
+	}
+
 	router.Initialize()
 }
